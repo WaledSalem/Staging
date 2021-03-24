ee48379826b8755fb40b0e20873134f53e383ebc
Serin Jeon <serin0837@yahoo.com>
2021-03-24 14:01:14 +0000

    aws instance work with public ip and add public ip output.tf

--- cluster-controller/main.tf
+++ cluster-controller/main.tf
@@ -30,23 +30,72 @@ resource "aws_subnet" "internal" {
   }
  }
 
-//need to look here again------------------
+
 #internet gate way
-# resource "aws_internet_gateway" "gw" {
-#   vpc_id = aws_vpc.main.id
-# }
-# #public ip
-# resource "aws_eip" "main" {
-#   network_interface = aws_network_interface.main.id
-#   //associate_with_private_ip = "10.0.1.50"
-#   vpc      = true
-#   depends_on = [aws_internet_gateway.gw]
-# }
+resource "aws_internet_gateway" "gw" {
+   vpc_id = aws_vpc.main.id
+}
+
+#aws_route_table
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "Prod"
+  }
+}
+
+resource "aws_security_group" "allow_web" {
+  name = "allow_web_traffic"
+  description = "Allow web inbound trafiic"
+  vpc_id = aws_vpc.main.id
+
+  ingress{
+    description= "HTTPS"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow web"
+  }
+}
+
+#allow route table association
+resource "aws_route_table_association" "a" {
+  subnet_id = aws_subnet.internal.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+ #public ip
+resource "aws_eip" "main" {
+   network_interface = aws_network_interface.main.id
+   associate_with_private_ip = "10.0.1.50"
+   vpc      = true
+   depends_on = [aws_internet_gateway.gw]
+ }
 #network interface
 resource "aws_network_interface" "main" {
    subnet_id       = aws_subnet.internal.id
    private_ips     = ["10.0.1.50"]//any id whithin the subnet ip
-   //security_groups = [aws_security_group.allow_web.id]
+   security_groups = [aws_security_group.allow_web.id]
    tags = {
      Name = "kube_network_interface"
    }
