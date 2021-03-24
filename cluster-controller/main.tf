099a444c107da95f38d510b7e491307416f75bd3
Serin Jeon <serin0837@yahoo.com>
2021-03-24 11:41:41 +0000

    aws instance work

--- cluster-controller/main.tf
+++ cluster-controller/main.tf
@@ -1,14 +1,16 @@
+variable "access_key" {
+
+}
+variable "secret_key" {
+  
+}
+
 #configure aws provider 
 #set access_key and secret_key later
 provider "aws"{
-    access_key = ""
-    secret_key = ""
+    access_key = var.access_key
+    secret_key = var.secret_key
     region = "eu-west-2"
-}
-
-#create resource group
-resource "aws_resourcegroups_group" "main" {
-    name = "kube-controller"
 }
 
 #virtual network
@@ -22,24 +24,45 @@ resource "aws_vpc" "main"{
 #subnet
 resource "aws_subnet" "internal" {
   vpc_id = aws_vpc.main.id
-  cidr_block= "10.0.2.0/24"
-  //availability_zone = "eu-west-2a"//need? 
+  cidr_block= "10.0.1.0/24"
   tags = {
      Name = "internal"
   }
  }
 
 //need to look here again------------------
-#public ip
-resource "aws_eip" "main" {
-  network_interface = aws_network_interface.main.id
-  //associate_with_private_ip = "10.0.1.50"
-  vpc      = true
-  depends_on = [aws_internet_gateway.gw]
-}
+#internet gate way
+# resource "aws_internet_gateway" "gw" {
+#   vpc_id = aws_vpc.main.id
+# }
+# #public ip
+# resource "aws_eip" "main" {
+#   network_interface = aws_network_interface.main.id
+#   //associate_with_private_ip = "10.0.1.50"
+#   vpc      = true
+#   depends_on = [aws_internet_gateway.gw]
+# }
 #network interface
 resource "aws_network_interface" "main" {
-  subnet_id       = aws_subnet.internal.id
-  private_ips     = ["10.0.1.50"]//any id whithin the subnet ip
-  security_groups = [aws_security_group.allow_web.id]
+   subnet_id       = aws_subnet.internal.id
+   private_ips     = ["10.0.1.50"]//any id whithin the subnet ip
+   //security_groups = [aws_security_group.allow_web.id]
+   tags = {
+     Name = "kube_network_interface"
+   }
+}
+//---------------
+
+#create VM
+resource "aws_instance" "main" {
+  ami = "ami-096cb92bb3580c759"
+  instance_type= "t2.micro"
+  
+  network_interface {
+    network_interface_id = aws_network_interface.main.id
+    device_index = 0
+  }
+  tags = {
+    Name = "kube-vm"
+  }
 }
