80ef1565004f133aefd8c3efd0d079399fd392d6
Suban <sabina.ku@hotmail.com>
2021-03-25 12:45:06 +0000

    sabina jenkins update

--- Jenkins/main.tf
+++ Jenkins/main.tf
@@ -1,41 +1,22 @@
+variable "access_key" {
+}
+variable "secret_key" {
+}
 provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  version = "~> 2.0"
   region = "eu-west-1"
 }
-
-resource "aws_default_vpc" "default" {
-  tags = {
-    Name = "Default VPC"
-  }
-}
-
-#create resource group  ##resource 1##
-resource "aws_resourcegroups_group" "main" {
-  name     = "jenkins-resources"
-  #location = "eu-west-2a" 
-    resource_query {
-    query = <<JSON
-{
-  "ResourceTypeFilters": [
-    "AWS::EC2::Instance"
-  ],
-  "TagFilters": [
-    {
-      "Key": "Stage",
-      "Values": ["Test"]
-    }
-  ]
-}
-JSON
-  }
-}
- ##============================================
-
 resource "aws_vpc" "main" {
   cidr_block          = "10.0.0.0/16"  
   instance_tenancy    = "default"  
+  tags = {
+     Name = "jenkins-resources-vnet"
+   }
 }
 
- ##============================================
+##============================================
  #create subnet ##resource 3##
  
 resource "aws_subnet" "internal" {
@@ -45,36 +26,40 @@ resource "aws_subnet" "internal" {
     Name = "jenkins-internal"
   }
 }
-
-
- ##============================================
- #create public IP ##resource 4##
-
-resource "aws_eip" "main" {
-  instance            = aws_instance.main.id
-  vpc                 = true
+#internet gate way
+resource "aws_internet_gateway" "gw" {
+   vpc_id = aws_vpc.main.id
 }
+#aws_route_table
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+    route {
+    ipv6_cidr_block = "::/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "Prod"
+  }
+}
+##============================================
+#create public IP ##resource 4##
 
-
- ##============================================
-#create NIC ##resource 5##
-resource "aws_network_interface" "main" {
-  subnet_id       = aws_subnet.internal.id
-  private_ips     = ["10.0.1.10"] #["10.0.0.50"]
-  security_groups = [aws_security_group.main.id]
-
- }
 
 
 ##============================================
 
+
+##============================================
 ##resource 6##
 
-resource "aws_security_group" "main" {
+resource "aws_security_group" "jenkins" {
 
   description = "Allow inbound traffic"
   vpc_id      = aws_vpc.main.id
-
 
   ingress {
     from_port   = 8080
@@ -96,18 +81,56 @@ resource "aws_security_group" "main" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-##============================================
-  #Create VM ##resource 7##
-resource "aws_instance" "main" {
-  ami           = "ami-02df9ea15c1778c9c"
-  instance_type = "t2.micro"
-  #vpc_security_group_ids = [aws_security_group.main.id]
+  
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "jenkins"
   }
 
+}
 
-  #========================================
-  ##??##
+resource "aws_route_table_association" "a" {
+  subnet_id = aws_subnet.internal.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+resource "aws_eip" "main" {
+   network_interface = aws_network_interface.main.id
+   associate_with_private_ip = "10.0.1.50"
+   vpc      = true
+   depends_on = [aws_internet_gateway.gw]
+ }
+
+ resource "aws_network_interface" "main" {
+   subnet_id       = aws_subnet.internal.id
+   private_ips     = ["10.0.1.50"]//any id whithin the subnet ip
+   security_groups = [aws_security_group.jenkins.id]
+   tags = {
+     Name = "kube_network_interface"
+   }
+}
+##============================================
+#Create VM ##resource 7##
+resource "aws_instance" "main" {
+  ami           = "ami-096cb92bb3580c759"
+  instance_type = "t2.medium"
+    key_name = "project"//your key
+  network_interface {
+    network_interface_id = aws_network_interface.main.id
+    device_index = 0
+  #vpc_security_group_ids = [aws_security_group.main.id]
+  }
+    tags = {
+    Name = "jenkins"
+  }
+}
+
+#========================================
+##??##
 
 ##??##
